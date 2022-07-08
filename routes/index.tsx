a16d8d9661deba257c65843d422a67d80d60c7aa
DOMO <benwada@DOMOnoMac-mini.local>
2022-07-08 18:40:47 +0900

    lesson

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         src="/logo.svg"
         height="100px"
         alt="the fresh logo: a sliced lemon dripping with juice"
+        class={tw`flex`}
       />
       <p class={tw`my-6`}>
         Welcome to `fresh`. Try update this message in the ./routes/index.tsx
